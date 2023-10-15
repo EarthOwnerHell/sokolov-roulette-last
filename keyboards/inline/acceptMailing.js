26889fb292f8cc9fb60b9e61e480021ff251e91e
EarthOwnerHell <78320256+EarthOwnerHell@users.noreply.github.com>
2023-10-15 23:47:56 +0300

    ssdsfds

--- keyboards/inline/acceptMailing.js
+++
@@ -1,8 +0,0 @@
-const { textButton, keyboard, green, red } = require("../helpers")
-
-const noButtonBoard = keyboard([
-    [textButton({ label: 'ПОДТВЕРДИТЬ', payload: 'accept', color: green })],
-    [textButton({ label: 'ОТМЕНА', payload: 'no', color: red })],
-]).inline();
-
-module.exports = noButtonBoard
