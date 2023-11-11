444230243bad5c1aa29fefed5f31fdf163dca8af
EarthOwnerHell <78320256+EarthOwnerHell@users.noreply.github.com>
2023-11-12 01:13:40 +0300

    Update generateCombination.js

--- pages/gameCommands/generateCombination.js
+++ pages/gameCommands/generateCombination.js
@@ -27,7 +27,7 @@ const randomDependingMode = {
     return [coefficent]
     },
     'l7m' : function(){
-    const number = getRandomValue(1, 7)
+    const number = getRandomValue(2, 12)
     const numberType = number < 7 ? 'less' : number > 7 ? 'more' : 'seven'
     return [number, numberType]
     },
