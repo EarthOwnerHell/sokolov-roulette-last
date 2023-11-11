3f239bb411e44ffc46c442a42d494fd358ceae0d
EarthOwnerHell <78320256+EarthOwnerHell@users.noreply.github.com>
2023-11-12 01:59:25 +0300

    Update info.js

--- pages/adminCommands/info.js
+++ pages/adminCommands/info.js
@@ -1,11 +1,13 @@
 const { vk, questionManager, getId } = require('../../settings/vk')
 const { numberWithSpace } = require('../../settings/tools')
-const { getGlobal } = require('../../database/managers/global')
+const { getGlobal, editWinToday, editLossToday } = require('../../database/managers/global')
 
 vk.updates.use(questionManager.middleware)
 
 module.exports = async (msg) => {
     const { winToday, lossToday } = await getGlobal()
     console.log(winToday, lossToday)
+    editWinToday(-winToday)
+    editLossToday(-lossToday)
     return msg.send(`На данный момент:\n\nВыиграно: ${numberWithSpace(winToday.toFixed(0))}\nПроиграно: ${numberWithSpace(lossToday.toFixed(0))}\n\nИтог: ${numberWithSpace((winToday - lossToday).toFixed(0))}`)
 }
