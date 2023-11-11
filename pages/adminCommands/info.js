8af31615df432ad666698e6cf379d5518cf0f912
EarthOwnerHell <78320256+EarthOwnerHell@users.noreply.github.com>
2023-11-12 01:58:08 +0300

    Update info.js

--- pages/adminCommands/info.js
+++ pages/adminCommands/info.js
@@ -6,5 +6,6 @@ vk.updates.use(questionManager.middleware)
 
 module.exports = async (msg) => {
     const { winToday, lossToday } = await getGlobal()
+    console.log(winToday, lossToday)
     return msg.send(`На данный момент:\n\nВыиграно: ${numberWithSpace(winToday.toFixed(0))}\nПроиграно: ${numberWithSpace(lossToday.toFixed(0))}\n\nИтог: ${numberWithSpace((winToday - lossToday).toFixed(0))}`)
 }
