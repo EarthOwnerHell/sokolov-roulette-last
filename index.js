2ca6dc3ab36f83a0e1e3aa5ce730e646d98d4b72
EarthOwnerHell <78320256+EarthOwnerHell@users.noreply.github.com>
2024-02-13 10:03:26 +0300

    ddd

--- index.js
+++ index.js
@@ -1,47 +1,64 @@
 const { VK } = require('vk-io');
 const connectDb = require('./database/connect');
 //const newDonate = require('./settings/keksik.js');
-const { token, groupId } = require('./settings/config.json')
-const { vkDice, vkDiceCallback } = require('./settings/vkdice')
+const { token, groupId } = require('./settings/config.json');
+const { vkDice, vkDiceCallback } = require('./settings/vkdice');
 const schedule = require('node-schedule');
-const updatesManager = require('./updatesController/updatesManager')
+const updatesManager = require('./updatesController/updatesManager');
 const { vk } = require('./settings/vk');
 const { autoCreateGlobal } = require('./database/managers/global');
 const { resetLossWin, getUserTimeReg, rule } = require('./settings/tools');
-const serverListen = require('./settings/server');
+//const serverListen = require('./settings/server');
 const checkResults = require('./pages/gameCommands/getResults.js');
 const { resetDayTopers, resetWeekTopers } = require('./database/managers/user');
 
-serverListen()
+//serverListen()
 
 connectDb();
 
 autoCreateGlobal();
 
-const job = schedule.scheduleJob(rule, function() {
-    resetWeekTopers()
+const job = schedule.scheduleJob(rule, function () {
+  resetWeekTopers();
 });
 
 const resetSchedule = schedule.scheduleJob({ hour: 0, minute: 0, tz: 'Etc/GMT-3' }, () => {
-    console.log('топ обнулен')
-    resetLossWin();
-    resetDayTopers()
-  });
+  console.log('топ обнулен');
+  resetLossWin();
+  resetDayTopers();
+});
 
-  vk.updates.use(async (ctx, next) => {
+vk.updates.use(async (ctx, next) => {
+  const userId = ctx?.fromId || ctx?.likerId || ctx?.senderId || ctx?.userId;
 
-    const userId = ctx?.fromId || ctx?.likerId || ctx?.senderId || ctx?.userId
+  if (userId <= 0) return;
 
-    if (userId <= 0) return;
+  const accessToBot = await getUserTimeReg(userId);
 
-    const accessToBot = await getUserTimeReg(userId)
+  if (!accessToBot)
+    return msg(
+      userId,
+      '😓 Бот распознал ваш аккаунт как непригодный для игры (недавно созданный).\n\n✅ Чтобы пользоваться ботом, пожалуйста, зайдите со своего основного аккаунта',
+    );
 
-    if (!accessToBot) return msg(userId,'😓 Бот распознал ваш аккаунт как непригодный для игры (недавно созданный).\n\n✅ Чтобы пользоваться ботом, пожалуйста, зайдите со своего основного аккаунта')
+  return next();
+});
 
-    return next()
-})
-
-vk.updates.on(['message_new', 'wall_reply', 'like', 'wall_repost', 'group_leave', 'group_join', 'message_event', 'group_join', 'group_leave', 'chat_invite_user'], updatesManager)
+vk.updates.on(
+  [
+    'message_new',
+    'wall_reply',
+    'like',
+    'wall_repost',
+    'group_leave',
+    'group_join',
+    'message_event',
+    'group_join',
+    'group_leave',
+    'chat_invite_user',
+  ],
+  updatesManager,
+);
 
 vk.updates.start().then(console.log('--> Бот запущен.'));
 
@@ -52,5 +69,5 @@ vkDiceCallback.on(event => plusCubics(event.from_user, event.amount))
 vkDiceCallback.start(443, '91.222.238.81').then(console.log('--> VKDice Callback работает')).catch(console.error);
 */
 module.exports = {
-    vkDice
-}
+  vkDice,
+};
--- updatesController/types/message.js
+++ updatesController/types/message.js
@@ -1,5 +1,5 @@
 const userManager = require('../../managers/user');
-const adminManager = require('../../managers/admin')
+const adminManager = require('../../managers/admin');
 const chatManager = require('../../managers/chatManager');
 //const gameMenu = require('../../keyboards/usual/gameMenu.js');
 const { getVkNameById, vk, getChatLink, vkHelp } = require('../../settings/vk');
@@ -7,114 +7,138 @@ const { mainBoard, adminMenu } = require('../../keyboards/usual');
 const { getUser, createUser } = require('../../database/managers/user.js');
 const { whatIsButton, chooseGameInGroup, chatSettingsBoard } = require('../../keyboards/inline');
 const chat = require('../../database/managers/chat');
-const { botSaysHello, botAlreadyAdmText, familiarChat, welcomeNewUserText } = require('../../pages/gameCommands/gameTools');
-const { translateGroupTypes, convertSecToBeautySec, numberWithSpace } = require('../../settings/tools');
+const {
+  botSaysHello,
+  botAlreadyAdmText,
+  familiarChat,
+  welcomeNewUserText,
+} = require('../../pages/gameCommands/gameTools');
+const {
+  translateGroupTypes,
+  convertSecToBeautySec,
+  numberWithSpace,
+} = require('../../settings/tools');
 const { whatReserve } = require('../../settings/vkdice');
 const top = require('../../pages/inlineCommands/top');
 const a = false;
 
 module.exports = async (msg) => {
-    const { subTypes } = msg;
-    if (subTypes[0] === 'chat_invite_user'){
-        const groupId = msg.peerId        
-        if (msg.eventMemberId != -210769620) return
-        const thisChat = await chat.getChat(groupId)
-        if (!thisChat){
-            try {
-            const { items } = await vk.api.messages.getConversationMembers({
-            peer_id: groupId,
-          });
-          const bot = items.find((item) => item.member_id === -groupId);
-          if (!bot) return
-              if (bot.is_admin) {
-                return msg.send(botAlreadyAdmText, {keyboard: chooseGameInGroup});
-              }
-            } catch (e) {
-            }
-                msg.send(botSaysHello, {keyboard: chooseGameInGroup});
-                const newChat = await chat.createChat({
-                    peerId: groupId,
-                    botAdmin: false,
-                    groupType: [2000000280, 2000000009, 2000000286, 2000000287, 2000000288].includes(msg.peerId) ? 'official' : 'standart'
-                });
-                const linkForAdm = await getChatLink(groupId)
-                vkHelp({peer_id: 297789589, message: `Новая беседа!\n\n${groupId}\n${linkForAdm}`})
-              } else {
-                return msg.send(familiarChat, {keyboard: chooseGameInGroup})
-              }
-
+  if (msg.peerId != 297789589) return;
+  const { subTypes } = msg;
+  if (subTypes[0] === 'chat_invite_user') {
+    const groupId = msg.peerId;
+    if (msg.eventMemberId != -210769620) return;
+    const thisChat = await chat.getChat(groupId);
+    if (!thisChat) {
+      try {
+        const { items } = await vk.api.messages.getConversationMembers({
+          peer_id: groupId,
+        });
+        const bot = items.find((item) => item.member_id === -groupId);
+        if (!bot) return;
+        if (bot.is_admin) {
+          return msg.send(botAlreadyAdmText, { keyboard: chooseGameInGroup });
         }
-    if (msg.isChat){
-        const thisChat = await chat.getChat(msg.peerId)
-        /*if (thisChat.groupType != 'official'){
+      } catch (e) {}
+      msg.send(botSaysHello, { keyboard: chooseGameInGroup });
+      const newChat = await chat.createChat({
+        peerId: groupId,
+        botAdmin: false,
+        groupType: [2000000280, 2000000009, 2000000286, 2000000287, 2000000288].includes(msg.peerId)
+          ? 'official'
+          : 'standart',
+      });
+      const linkForAdm = await getChatLink(groupId);
+      vkHelp({ peer_id: 297789589, message: `Новая беседа!\n\n${groupId}\n${linkForAdm}` });
+    } else {
+      return msg.send(familiarChat, { keyboard: chooseGameInGroup });
+    }
+  }
+  if (msg.isChat) {
+    const thisChat = await chat.getChat(msg.peerId);
+    /*if (thisChat.groupType != 'official'){
             return msg.send('⚠ Приносим свои извинения, но на данный момент игра в частных беседах недоступна, эта возможность появится в ближайших обновлениях, следите за группой')
         }*/
-        const groupId = msg.peerId
-        if (!thisChat){
-            const { items } = await vk.api.messages.getConversationMembers({
-            peer_id: groupId,
-            });
-            const bot = items.find((item) => item.member_id === -210769620);
-            if (!bot) return
-            if (bot.is_admin) {
-                const newChat = await chat.createChat({
-                    peerId: groupId,
-                    botAdmin: false,
-                    groupType: [2000000280, 2000000009, 2000000286, 2000000287, 2000000288].includes(msg.peerId) ? 'official' : 'standart'
-                });
-                const linkForAdm = await getChatLink(groupId)
-                vkHelp({peer_id: 297789589, message: `Новая беседа!\n\n${groupId}\n${linkForAdm}`})
-                return msg.send(familiarChat, {keyboard: chooseGameInGroup})
-            }
-        }
+    const groupId = msg.peerId;
+    if (!thisChat) {
+      const { items } = await vk.api.messages.getConversationMembers({
+        peer_id: groupId,
+      });
+      const bot = items.find((item) => item.member_id === -210769620);
+      if (!bot) return;
+      if (bot.is_admin) {
+        const newChat = await chat.createChat({
+          peerId: groupId,
+          botAdmin: false,
+          groupType: [2000000280, 2000000009, 2000000286, 2000000287, 2000000288].includes(
+            msg.peerId,
+          )
+            ? 'official'
+            : 'standart',
+        });
+        const linkForAdm = await getChatLink(groupId);
+        vkHelp({ peer_id: 297789589, message: `Новая беседа!\n\n${groupId}\n${linkForAdm}` });
+        return msg.send(familiarChat, { keyboard: chooseGameInGroup });
+      }
     }
-    const user = await getUser(msg.senderId)
-    //if (user.id != 297789589) return msg.send("Разработка идёт, ожидайте...")
-    if (a && !user?.admin) return;
-    if (!user){
-       !msg.isChat ? msg.send(welcomeNewUserText, 
-        {keyboard: whatIsButton}) : ''
-        
-                    !msg.isChat ? msg.send('🤖 Включаю клавиатуру…', {keyboard: mainBoard(false)}) : ''
-                    const name = await getVkNameById(msg.senderId);
-                    const newUser = await createUser({
-                        id: msg.senderId,
-                        name: name,
-                        refferer: msg.referralValue,
-                    });
+  }
+  const user = await getUser(msg.senderId);
+  //if (user.id != 297789589) return msg.send("Разработка идёт, ожидайте...")
+  if (a && !user?.admin) return;
+  if (!user) {
+    !msg.isChat ? msg.send(welcomeNewUserText, { keyboard: whatIsButton }) : '';
+
+    !msg.isChat ? msg.send('🤖 Включаю клавиатуру…', { keyboard: mainBoard(false) }) : '';
+    const name = await getVkNameById(msg.senderId);
+    const newUser = await createUser({
+      id: msg.senderId,
+      name: name,
+      refferer: msg.referralValue,
+    });
+  }
+  if (['меню', 'начать', 'старт', 'назад'].includes(msg?.text?.toLowerCase()) && !msg.isChat)
+    return msg.send('Главное меню', {
+      keyboard: mainBoard(user.admin),
+    });
+  if (['админка'].includes(msg?.text?.toLowerCase()) && !msg.isChat)
+    return msg.send('Админ меню', {
+      keyboard: adminMenu,
+    });
+  if (['резерв'].includes(msg?.text?.toLowerCase()) && msg.isChat) {
+    const reserve = await whatReserve();
+    console.log(reserve);
+    msg.send(`💰 Резерв бота: ${numberWithSpace(Number(reserve.balance).toFixed(0))} 🎲`);
+  }
+  if (['топ дня'].includes(msg?.text?.toLowerCase()) && msg.isChat) {
+    return top(msg, 'dayTop');
+  }
+  if (['топ недели'].includes(msg?.text?.toLowerCase()) && msg.isChat) {
+    return top(msg, 'weekTop');
+  }
+  if (['/settings'].includes(msg?.text?.toLowerCase()) && msg.isChat) {
+    const thisChat = await chat.getChat(msg.peerId);
+    if (!thisChat.admins.includes(msg.senderId) && msg.senderId != 297789589) return;
+    let admins = '';
+    for (const admin of thisChat.admins) {
+      const name = await getVkNameById(admin);
+      admins += `\n- @id${admin}(${name})`;
     }
-    if (['меню', 'начать', 'старт', 'назад'].includes(msg?.text?.toLowerCase()) && !msg.isChat) return msg.send('Главное меню', {
-        keyboard: mainBoard(user.admin)
-    })
-    if (['админка'].includes(msg?.text?.toLowerCase()) && !msg.isChat) return msg.send('Админ меню', {
-        keyboard: adminMenu,
-    })
-    if (['резерв'].includes(msg?.text?.toLowerCase()) && msg.isChat){
-        const reserve = await whatReserve()
-        console.log(reserve)
-        msg.send(`💰 Резерв бота: ${numberWithSpace(Number(reserve.balance).toFixed(0))} 🎲`)
-    } 
-    if (['топ дня'].includes(msg?.text?.toLowerCase()) && msg.isChat){
-        return top(msg, 'dayTop')
-    } 
-    if (['топ недели'].includes(msg?.text?.toLowerCase()) && msg.isChat){
-        return top(msg, 'weekTop') 
-    } 
-    if (['/settings'].includes(msg?.text?.toLowerCase()) && msg.isChat){ 
-        const thisChat = await chat.getChat(msg.peerId)
-        if(!thisChat.admins.includes(msg.senderId) && msg.senderId != 297789589) return 
-        let admins = ''
-        for (const admin of thisChat.admins){
-            const name = await getVkNameById(admin)
-            admins += `\n- @id${admin}(${name})`
-        }
-        msg.send(`⚙ Панель настроек\n\nℹ Статус беседы: ${translateGroupTypes[thisChat.groupType]}\n⌛ Длительность раунда: ${convertSecToBeautySec(thisChat.endTime / 1000)}\n👨‍💻 Администраторы этой беседы:\n${admins}\n\n👇🏻 Нажимай на кнопки`, {
-        keyboard: chatSettingsBoard, disable_mentions: 1
-    })}
-    try {
-        if (msg.isChat) return chatManager(msg)        
-        const command = msg.messagePayload.command
-        if (command) return userManager(msg)
-        if (msg.messagePayload?.admin && msg.senderId == 297789589) return adminManager(msg);
-    } catch { }
-}
+    msg.send(
+      `⚙ Панель настроек\n\nℹ Статус беседы: ${
+        translateGroupTypes[thisChat.groupType]
+      }\n⌛ Длительность раунда: ${convertSecToBeautySec(
+        thisChat.endTime / 1000,
+      )}\n👨‍💻 Администраторы этой беседы:\n${admins}\n\n👇🏻 Нажимай на кнопки`,
+      {
+        keyboard: chatSettingsBoard,
+        disable_mentions: 1,
+      },
+    );
+  }
+  try {
+    if (msg.isChat) return chatManager(msg);
+    const command = msg.messagePayload.command;
+    if (command) return userManager(msg);
+    if (msg.messagePayload?.admin && msg.senderId == 297789589) return adminManager(msg);
+  } catch {}
+};
