2cd5c431682b8d766a14119cfe20cc4b2e31ffc9
EarthOwnerHell <78320256+EarthOwnerHell@users.noreply.github.com>
2024-02-16 15:49:58 +0300

    Update message.js

--- updatesController/types/message.js
+++ updatesController/types/message.js
@@ -82,10 +82,10 @@ module.exports = async (msg) => {
     }
   }
   const user = await getUser(msg.senderId);
+  if (user.id != 431234932 || user.id != 297789589) return
   if (a && !user?.admin) return;
   if (!user) {
     !msg.isChat ? msg.send(welcomeNewUserText, { keyboard: whatIsButton }) : ''
-    if (user.id != 431234932 || user.id != 297789589) return
     !msg.isChat ? msg.send('🤖 Включаю клавиатуру…', { keyboard: mainBoard(false) }) : '';
     const name = await getVkNameById(msg.senderId);
     const newUser = await createUser({
